3de1dac5f81bd1588187cf77e76b137cf47f99b4
Rishabh-mac <raj.rishabh01@gmail.com>
2022-02-12 22:00:24 +0530

    Arrays : In place operations and random problems

--- src/com/ds/array/Array_Conclusion.java
+++ src/com/ds/array/Array_Conclusion.java
@@ -1,4 +1,33 @@
 package com.ds.array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Array_Conclusion {
+    public static void main(String[] args) {
+        //1 Height Checker
+        int[] heights = {1,8,3,7,2,4};
+        System.out.println("The mismatch for heights is at count : " + heightChecker(heights));
+    }
+
+    //1 Height Checker
+    private static int heightChecker(int[] A) {
+        int[] map = new int[101]; // heights go from 1 to 100
+        for(int h: A) ++map[h];
+
+        int res = 0;
+        int h_ptr = 1;
+
+        for(int h: A) {
+            while(map[h_ptr] == 0) ++h_ptr;
+
+            if(h_ptr != h) {
+                ++res;
+            }
+
+            --map[h_ptr];
+        }
+
+        return res;
+    }
 }
